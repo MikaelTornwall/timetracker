92c462277a5a2d2335189439dbf1145340462d7b
Mikael Törnwall <mikael.tornwall@aalto.fi>
2019-08-14 20:29:08 +0300

    add authorization to course template
    add authorization to course template so that the view is different for
    different roles, clean code

--- application/courses/views.py
+++ application/courses/views.py
@@ -6,6 +6,7 @@ from application.courses.models import Course
 from application.auth.models import User
 from application.courses.forms import CourseForm
 
+# Views for students
 @app.route("/courses/", methods=["GET"])
 @login_required(role="STUDENT")
 def courses_index():
@@ -14,25 +15,6 @@ def courses_index():
     length = Course.count_courses()
     return render_template("courses/courses.html", courses = c, mycourses = m, length = length)
 
-@app.route("/courses/mycourses", methods=["GET"])
-@login_required(role="TEACHER")
-def courses_mycourses():
-    c = User.query.get(current_user.id)
-    length = c.count_users_courses(current_user)
-    return render_template("courses/mylist.html", courses = c.courses, length = length)
-
-
-@app.route("/courses/new/")
-@login_required(role="TEACHER")
-def courses_form():
-    return render_template("courses/new.html", form = CourseForm())
-
-@app.route("/courses/<course_id>/", methods=["GET"])
-@login_required()
-def courses_course(course_id):
-    c = Course.query.get(course_id)
-    return render_template("courses/course.html", course = c)
-
 @app.route("/courses/enroll/<course_id>/", methods=["GET"])
 @login_required(role="STUDENT")
 def course_enroll(course_id):
@@ -40,13 +22,33 @@ def course_enroll(course_id):
     u = User.query.get(current_user.id)
 
     if u in c.users:
-        c.users.remove(u)        
+        c.users.remove(u)
     else:
         c.users.append(u)
 
     db.session.commit()
 
     return redirect(url_for("courses_index"))
+
+# Views for teachers and students
+@app.route("/courses/<course_id>/", methods=["GET"])
+@login_required()
+def courses_course(course_id):
+    c = Course.query.get(course_id)
+    return render_template("courses/course.html", course = c)
+
+# Views for teachers
+@app.route("/courses/mycourses", methods=["GET"])
+@login_required(role="TEACHER")
+def courses_mycourses():
+    c = User.query.get(current_user.id)
+    length = c.count_users_courses(current_user)
+    return render_template("courses/mylist.html", courses = c.courses, length = length)
+
+@app.route("/courses/new/")
+@login_required(role="TEACHER")
+def courses_form():
+    return render_template("courses/new.html", form = CourseForm())
 
 @app.route("/courses/", methods=["POST"])
 @login_required(role="TEACHER")
@@ -59,7 +61,6 @@ def courses_create():
     c = Course(form.courseId.data, form.title.data, form.description.data, form.duration.data, form.deadline.data)
     u = User.query.get(current_user.id)
 
-    # u.courses.append(c)
     c.users.append(u)
 
     db.session().add(c)
@@ -91,16 +92,16 @@ def courses_update(course_id):
 
     db.session().commit()
 
-    return redirect(url_for("courses_index", course_id = course_id))
+    return redirect(url_for("courses_course", course_id = course_id))
 
 @app.route("/courses/<course_id>/delete/", methods=["POST"])
 @login_required(role="TEACHER")
 def courses_delete(course_id):
     print('id: ' + course_id)
     c = Course.query.filter_by(id=course_id).first()
-    #c.users.clear()
+
     if (c is not None):
         db.session.delete(c)
         db.session.commit()
 
-    return redirect(url_for("courses_index"))
+    return redirect(url_for("courses_mycourses"))
